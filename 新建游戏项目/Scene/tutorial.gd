2706d108efa5c624d8714e3040fe805aa70c77ed
CalvinChen212 <lchen81@sva.edu>
2025-03-24 16:47:26 -0400

    Added final project

---
+++ 新建游戏项目/Scene/tutorial.gd
@@ -0,0 +1,17 @@
+extends Button
+
+@onready var button = $"."
+
+func _ready():
+	button.connect("mouse_entered", _on_mouse_entered)
+	button.connect("mouse_exited", _on_mouse_exited)
+	button.connect("pressed", _on_button_pressed)
+
+func _on_mouse_entered():
+	button.modulate = Color(1.2, 1.2, 1.2)
+
+func _on_mouse_exited():
+	button.modulate = Color(1, 1, 1)
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://tutorial.tscn")
